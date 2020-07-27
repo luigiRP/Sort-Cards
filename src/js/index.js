7afe4f559d4fc43c16abff99b1b0c00c07a8006e
luigiRP <luigirosales84@gmail.com>
2020-07-27 13:16:05 +0000

    first

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,24 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let medio = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Q", "J", "K"];
+  let medioNumero = Math.floor(Math.random() * medio.length);
+  let palo = document.querySelector("#medioSimbolo");
+  palo.innerHTML = medio[medioNumero];
+
+  let inicio = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+  let inicioNumero = Math.floor(Math.random() * inicio.length);
+
+  let inicioSimbolo = document.querySelector("#inicioSimbolo");
+  let finalSimbolo = document.querySelector("#finalSimbolo");
+
+  inicioSimbolo.innerHTML = inicio[inicioNumero];
+  finalSimbolo.innerHTML = inicio[inicioNumero];
+  finalSimbolo.style.transform = "rotate(180deg)";
+
+  if (inicio[inicioNumero] == "&hearts;" || inicio[inicioNumero] == "&diams;") {
+    inicioSimbolo.style.color = "red";
+    finalSimbolo.style.color = "red";
+    palo.style.color = "red";
+  }
 };
